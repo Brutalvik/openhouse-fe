4ca40e84ca437f2b54369af134cb18f3508daf3e
brutalvik <vikcanada90@gmail.com>
2023-12-21 13:59:40 -0700

    Added font family

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "App";
+
+//UI Library
+import { ChakraProvider } from "@chakra-ui/react";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container as HTMLElement);
 
-root.render(<App />);
+root.render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
+);
